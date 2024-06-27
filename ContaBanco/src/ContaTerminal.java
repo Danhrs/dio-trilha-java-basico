aef445b6b7d97cccf7a9d2a808093f8f6011bb92
Danhrs <danh.progm@gmail.com>
2024-06-27 11:33:40 -0300

    feat: "inclusão do projeto 'Controle de Fluxo'"

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,26 +1,14 @@
 
-import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-    
-        System.out.print("-------Olá bem-vindo(a) ao Banco DIO--------\n");
-        System.out.print("--Para criar uma conta siga as instruções!--\n");
+        double saldo = 18;
+        double valorSolicitado = 17;
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.next();
+        if (valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
         
-        System.out.println("Digite sua numero da conta: ");
-        int conta = scanner.nextInt();
+        System.out.println(saldo);
 
-        System.out.println("Digite o nome da agencia: ");
-        String agencia = scanner.next();
-
-        System.out.println("Digite o seu saldo: ");
-        double saldo = scanner.nextDouble();
-
-        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ conta +" e seu saldo R$"+ saldo +" já está disponível para saque");
     }
 }
